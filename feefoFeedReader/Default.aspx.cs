abbde71fd18ad40519092696daba8664c18fcd8e
manishawattal <wattalmanisha@gmail.com>
2015-01-02 18:00:19 +0530

    Feed Reader Project

---
+++ feefoFeedReader/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+
+namespace feefoFeedReader
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string xml_filename = "http://perf.isango.com/feefo.xml";
+            string xslPath = MapPath("feedback.xsl"); //xsl document in same working directory as this page
+
+
+
+            XPathDocument xpathDoc = new XPathDocument(xml_filename);
+            XslCompiledTransform transform = new XslCompiledTransform();
+            transform.Load(xslPath);
+            transform.Transform(xpathDoc, null, Response.Output);
+        }
+    }
+}
